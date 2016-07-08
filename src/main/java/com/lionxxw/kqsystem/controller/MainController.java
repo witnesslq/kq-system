ba16f013afd4e0c6697620f569f2a4b808b71918
wangjian <wangjian@baofoo.com>
2016-07-08 17:34:56 +0800

    add:当前个人信息的展示和修改

--- src/main/java/com/lionxxw/kqsystem/controller/MainController.java
+++ src/main/java/com/lionxxw/kqsystem/controller/MainController.java
@@ -1,7 +1,19 @@
 package com.lionxxw.kqsystem.controller;
 
+import com.lionxxw.kqsystem.code.constants.DataStatus;
+import com.lionxxw.kqsystem.code.model.Response;
+import com.lionxxw.kqsystem.code.utils.BeanUtils;
+import com.lionxxw.kqsystem.code.utils.ResponseUtils;
+import com.lionxxw.kqsystem.dto.UserDto;
+import com.lionxxw.kqsystem.mode.LoginUser;
+import com.lionxxw.kqsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 /**
  * 主面板控制层
@@ -11,9 +23,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController extends KqsController {
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 主页
+     * @return
+     */
     @RequestMapping(value = "main")
     public String index(){
-
         return "kqs/main";
+    }
+
+    /**
+     * 获取当前登录人信息
+     * @param request
+     * @param response
+     */
+    @RequestMapping(value = "getUser")
+    public void getUser(HttpServletRequest request, HttpServletResponse response){
+        LoginUser loginUser = getLoginUser(request);
+        ResponseUtils.renderJson(response, loginUser);
+    }
+
+    /**
+     * 更新当前用户信息
+     * @param request
+     * @param response
+     * @param user
+     */
+    @RequestMapping(value = "updateUser")
+    public void updateUser(HttpServletRequest request, HttpServletResponse response, UserDto user){
+        Response<String> res = new Response<String>();
+        try {
+            userService.update(user);
+            LoginUser loginUser = BeanUtils.createBeanByTarget(user, LoginUser.class);
+            LoginUser loginUser2 = getLoginUser(request);
+            loginUser.setLastLoginTime(loginUser2.getLastLoginTime());
+            loginUser.setLastLoginIp(loginUser2.getLastLoginIp());
+            sessionProvider.setAttribute(request, DataStatus.SESSION_USER, loginUser);
+            res.setMessage("个人信息修改成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setMessage(e.getMessage());
+            res.setStatus(DataStatus.HTTP_FAILE);
+        }
+        ResponseUtils.renderJson(response, res);
     }
 }
---
+++ src/main/webapp/skin/js/kqs/main.js
@@ -0,0 +1,81 @@
+/**
+ * Created by BF100252 on 2016/7/8.
+ */
+var _base = {
+    init:{
+        // 初始化界面
+        pageUI:function(){
+            $("#easyuitree").tree({
+                url: "/data/menu.json",
+                dnd:true, // 是否启动拖拽功能
+                animate:true, // 是否显示展开/折叠动画
+                checkbox:true,// 是否在每一个节点上显示复选框
+                lines:true, // 是否显示树控件上的虚线
+                onClick:function(node){
+                    console.log(node);
+                    // $("#easyuitree").tree("getChildren",node)
+                    var children = node.children; // 获取子节点
+//                            console.log(children);
+
+                    var tabs = $('#center_tabs').tabs("getTab", node.text);
+                    //console.info(tabs);
+                    if (tabs){
+                        // 切换到指定面板
+                        $('#center_tabs').tabs("select", node.text);
+                    }else{
+                        var content = '<iframe scrolling="auto" frameborder="0"  src="/WEB-INF/view/kqs/overtime/base_6.html" style="width:100%;height:100%;"></iframe>';
+                        // 添加面板
+                        $('#center_tabs').tabs('add',{
+                            title:node.text,
+                            //content:'Tab Body',
+                            content:content,
+                            closable:true
+                        });
+                    }
+                }
+            });
+        },
+        // 初始化需绑定的按钮
+        bindEvent:function(){
+            $("#eazyui-book").click(function () {
+                $('#dlg').dialog('open').dialog('setTitle','个人信息');
+                $('#fm').form('load','/kqs/getUser.do');
+            });
+            $("#eazyui-quit").click(function(){
+                $.messager.confirm('确认','您确认想要退出吗?',function(r){
+                    if (r){
+                        $.messager.alert('操作提示', "退出成功!", 'info',function(){
+                            window.location.href = '/logout.do'
+                        });
+                    }
+                });
+            });
+        }
+    }
+}
+
+function updateUser(){
+    $('#fm').form('submit',{
+        onSubmit:function(){
+            return $(this).form('validate');
+        },
+        success:function(data){
+            var jsondata = null;//eval('(' + data + ')');
+            if (data instanceof Object){
+                jsondata = data;
+            }else{
+                jsondata = eval('(' + data + ')');
+            }
+            console.log(jsondata);
+            console.log(jsondata.status);
+            console.log(jsondata.message);
+            if (jsondata.status == 200){
+                $.messager.alert('操作提示', jsondata.message, 'info',function(){
+                    $('#dlg').dialog('close');
+                });
+            }else{
+                $.messager.alert('操作提示', jsondata.message, 'error');
+            }
+        }
+    });
+}
